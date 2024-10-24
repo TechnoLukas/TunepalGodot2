5412444db9fc398f840d6368933803506acda9fa
technolukas <luklevus@outlook.com>
2024-10-24 13:37:55 +0200

    Implemented simple page switching + automatically updates page title.

--- scripts/menu_selector.gd
+++ scripts/menu_selector.gd
@@ -1,27 +1,30 @@
 extends Control
 
+# Main menu elements
 @onready var menu_button = $Container/container/menu_button
-@onready var deselect_button = $deselect_button
 @onready var menu = $Menu
 @onready var lable = $Container/container/label
-
 @export var title = ""
 
+# Swipe Animation Variables
+@onready var deselect_button = $deselect_button
 var swipe_right_speed = 4
 var swipe_left_speed = 6
-
 var current_menu_position = Vector2()
 var action = "" # swipe_right & swipe_left
-
-# transparency 1.0 to 0.2
-var transparency = 0.5
-
+var transparency = 0.5 # transparency 1.0 to 0.2
 var t = 0.0
 
+# Page selection Variables
+var pagenames = {
+				"record":{"node":"RecordPage","title":"Record"},
+				"keyword":{"node":"KeywordPage","title":"Search Tune"}
+				}
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	update_title()
+	
+func update_title():
 	lable.text = title
 
 func set_color(tr):
@@ -29,7 +32,6 @@ func set_color(tr):
 	style.bg_color=Color(0.0,0.0,0.0,tr)
 	deselect_button.add_theme_stylebox_override("normal",style)
 	deselect_button.add_theme_stylebox_override("hover",style)
-	#deselect_button.add_theme_stylebox_override("focus",style)
 	deselect_button.add_theme_stylebox_override("pressed",style)
 	deselect_button.add_theme_stylebox_override("disabled",style)
 	
@@ -56,17 +58,8 @@ func swipe_actions(delta):
 			menu.position=Vector2(-200,0)
 			action = ""
 	else:
-		t = 0.0	
-	
-func _process(delta: float) -> void:
-	swipe_actions(delta)
-			
-func _on_menu_button_pressed() -> void:
-	open_menu()
-	
-func _on_deselect_button_pressed() -> void:
-	close_menu()
-	
+		t = 0.0
+		
 func open_menu():
 	menu_button.disabled = true
 	deselect_button.disabled=false
@@ -78,13 +71,36 @@ func close_menu():
 	deselect_button.disabled=true
 	if t!=0: t = 1.0-t
 	action = "swipe_left"
+	
+func open_page(string):
+	var pages = get_parent().get_child(0).get_children()
+	for page in pages:
+		if page.name == pagenames[string]["node"]:
+			page.visible = true
+			title = pagenames[string]["title"]
+			update_title()
+		else:
+			page.visible = false
+	
+	
+func _process(delta: float) -> void:
+	swipe_actions(delta)
 
+
+
+func _on_menu_button_pressed() -> void:
+	open_menu()
+	
+func _on_deselect_button_pressed() -> void:
+	close_menu()
 
 func _on_record_scene_button_pressed() -> void:
 	close_menu()
+	open_page("record")
 
 func _on_keyword_scene_button_pressed() -> void:
 	close_menu()
+	open_page("keyword")
 
 func _on_randomtune_scene_button_pressed() -> void:
 	close_menu()
