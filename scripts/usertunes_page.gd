1a81ed609163bc8000a1d22411bedbcfc5fadafe
technolukas <luklevus@outlook.com>
2024-11-02 16:17:24 +0100

    Implemented "my tunes" pages. Works like bookmarks or favourites. No open/export functionality yet.

--- scripts/usertunes_page.gd
+++ scripts/usertunes_page.gd
@@ -1,21 +1,29 @@
 extends Control
 
+@onready var tunelist = $TunesList
 @onready var tunepage = $Tunepage
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	tunepage.return_button.visible=false
+	pass
 
 func showpage():
-	self.visible=true
-	var idx = randi_range(0, sqlite.tunes.size()-1)
-	var data = sqlite.tunes[idx]
-	tunepage.show_tune_page(data)
-	
-	
+	update_list()
+	self.visible = true
+
+
 func hidepage():
-	self.visible=false
+	self.visible = false
+	
+func update_list():
+	tunelist.clear_list()
+	var data = sqlite.user_tunes
+	for i in range(0,data.size()):
+		tunelist.add_item(data[i])
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_tunepage_returned() -> void:
+	update_list()
