56abfe72f7d85cdd4058b5ffc52fa2d318f641fb
Cormac Costello <117513353+CormacC30@users.noreply.github.com>
2025-03-31 22:10:51 +0100

    include linux build files and omit transcription engine

--- src/main.cpp
+++ src/main.cpp
@@ -17,12 +17,12 @@ void process_recording(const PackedByteArray& audioData) {
         Transcriber transcriber(audioData);
         
         // AI-based transcription
-        godot::String aiTranscription = String(transcriber.transcribeWithAI().c_str());
-        if (!aiTranscription.is_empty()) {
-            UtilityFunctions::print("AI Transcription: ", aiTranscription);
-        } else {
-            UtilityFunctions::print("AI Transcription failed");
-        }
+        // godot::String aiTranscription = String(transcriber.transcribeWithAI().c_str());
+        // if (!aiTranscription.is_empty()) {
+        //     UtilityFunctions::print("AI Transcription: ", aiTranscription);
+        // } else {
+        //     UtilityFunctions::print("AI Transcription failed");
+        // }
         
         // Traditional transcription
         godot::String traditionalTranscription = String(transcriber.transcribe(nullptr, nullptr, false).c_str());
